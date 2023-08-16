91d828227dff421ffe6717a05d4d8a7a52e3f089
hugo.liu <hugo.liu@siengine.com>
2023-08-16 14:16:51 +0800

    update tutorial01_first_window with LearnOpenGL chapter 1.3
    1. https://learnopengl.com/Getting-started/Hello-Window
    
    Signed-off-by: hugo.liu <hugo.liu@siengine.com>

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # dependencies
 
-1. `sudo apt install libx11-dev libxi-dev libgl1-mesa-dev libglu1-mesa-dev libxrandr-dev libxext-dev libxcursor-dev libxinerama-dev mesa-utils`
+1. `sudo apt install libx11-dev libxi-dev libgl1-mesa-dev libglu1-mesa-dev libxrandr-dev libxext-dev libxcursor-dev libxinerama-dev mesa-utils libxxf86vm-dev`
 2. tutorial: http://www.opengl-tutorial.org/beginners-tutorials/tutorial-1-opening-a-window/
 3. opengl 4.5 manual: https://registry.khronos.org/OpenGL-Refpages/gl4/html/
 4. check opengl version on ubuntu (need mesa-utils): `glxinfo | grep "OpenGL version"`
--- tutorial01_first_window/tutorial01.cpp
+++ tutorial01_first_window/tutorial01.cpp
@@ -13,6 +13,92 @@ GLFWwindow* window;
 #include <glm/glm.hpp>
 using namespace glm;
 
+/**
+ * defined in glfw/src/internal.h
+ * Window and context structure
+	struct _GLFWwindow
+	{
+		struct _GLFWwindow* next;
+
+		// Window settings and state
+		GLFWbool            resizable;
+		GLFWbool            decorated;
+		GLFWbool            autoIconify;
+		GLFWbool            floating;
+		GLFWbool            focusOnShow;
+		GLFWbool            mousePassthrough;
+		GLFWbool            shouldClose;
+		void*               userPointer;
+		GLFWbool            doublebuffer;
+		GLFWvidmode         videoMode;
+		_GLFWmonitor*       monitor;
+		_GLFWcursor*        cursor;
+
+		int                 minwidth, minheight;
+		int                 maxwidth, maxheight;
+		int                 numer, denom;
+
+		GLFWbool            stickyKeys;
+		GLFWbool            stickyMouseButtons;
+		GLFWbool            lockKeyMods;
+		int                 cursorMode;
+		char                mouseButtons[GLFW_MOUSE_BUTTON_LAST + 1];
+		char                keys[GLFW_KEY_LAST + 1];
+		// Virtual cursor position when cursor is disabled
+		double              virtualCursorPosX, virtualCursorPosY;
+		GLFWbool            rawMouseMotion;
+
+		_GLFWcontext        context;
+
+		struct {
+			GLFWwindowposfun          pos;
+			GLFWwindowsizefun         size;
+			GLFWwindowclosefun        close;
+			GLFWwindowrefreshfun      refresh;
+			GLFWwindowfocusfun        focus;
+			GLFWwindowiconifyfun      iconify;
+			GLFWwindowmaximizefun     maximize;
+			GLFWframebuffersizefun    fbsize;
+			GLFWwindowcontentscalefun scale;
+			GLFWmousebuttonfun        mouseButton;
+			GLFWcursorposfun          cursorPos;
+			GLFWcursorenterfun        cursorEnter;
+			GLFWscrollfun             scroll;
+			GLFWkeyfun                key;
+			GLFWcharfun               character;
+			GLFWcharmodsfun           charmods;
+			GLFWdropfun               drop;
+		} callbacks;
+
+		// This is defined in platform.h
+		GLFW_PLATFORM_WINDOW_STATE
+	};
+*/
+void framebuffer_size_callback(GLFWwindow* win, int width, int height)
+{
+	fprintf(stderr, "resize window: %dx%d\n", width, height);
+	glViewport(0, 0, width, height);
+}
+
+void window_close_callback(GLFWwindow* win)
+{
+	fprintf(stderr, "window %x closed\n", win);
+}
+
+void process_key_input(GLFWwindow* win)
+{
+	for(int key=GLFW_KEY_SPACE; key<GLFW_KEY_LAST; key+=1)
+	{
+		if(glfwGetKey(win, key) == GLFW_PRESS)
+		{
+			/* glfwGetKeyName not public in glfw-3.1 */
+			// int scancode = glfwGetKeyScancode(key);
+			// const char* keyname = glfwGetKeyName(key, 0);
+			fprintf(stderr, "key %d pressed\n", key);
+		}
+	}
+}
+
 int main(int argc, char **argv)
 {
 	for(int i = 0; i < argc; i++){
@@ -54,6 +140,10 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
+	glViewport(0, 0, 640, 480);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetWindowCloseCallback(window, window_close_callback);
+
 	/** until now GL context is ready, so print GL version */
     fprintf(stderr, "OpenGL version: %s \n", glGetString(GL_VERSION));
 
@@ -61,15 +151,29 @@ int main(int argc, char **argv)
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
 	// Dark blue background
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
-
+	// glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
+	float r = 0.0f;
+	float b = 1.0f;
 	do{
+		/* make a blue-to-green gradient */
+		r += 0.005f;
+		b -= 0.005f;
+		r = (r > 1.0f) ? 0.0f : r;
+		b = (b < 0.0f) ? 1.0f : b;
+		/**
+		 *  glClearColor() function is a state-setting function,
+		 *  and glClear() is a state-using function in that,
+		 *  it uses the current state to retrieve the clearing color from.
+		*/
+		glClearColor(r, 0.0f, b, 0.0f);
+
 		// Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
+		/* The possible bits we can set are GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT and GL_STENCIL_BUFFER_BIT. */
 		glClear( GL_COLOR_BUFFER_BIT );
 
 		// Draw nothing, see you in tutorial 2 !
+		process_key_input(window);
 
-		
 		// Swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
